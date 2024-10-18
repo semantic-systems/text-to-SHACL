426de393c6d3c5efd7692992f7dc5136d95ba625
Seike Appold <seike.appold@stud.leuphana.de>
2024-10-18 14:10:40 +0200

    Write function to check if save path already exists and build directory if necessary

--- scraping/0_extract_ars.py
+++ scraping/0_extract_ars.py
@@ -12,25 +12,34 @@
 import os
 import requests
 import pandas as pd
-from pathlib import Path
+from pathlib import Path check for file path in separate
+EXCEL_URL = "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis:bevoelkerungsstatistik:schluessel:rs_2024-10-31/download/Regionalschl_ssel_2024-10-31.xlsx"  
 
-def download_file(url: str, save_dir: str, filename: str) -> str:
+def save_path_exists(save_path: str, save_dir: str, activity: str) -> str:
     """
-    Download a file from the URL and save it to the specified directory.
-
-    :param url: URL of the file to download.
-    :param save_dir: Directory where the downloaded file will be saved.
-    :param file_name: Name of the file to save.
-    :return: If the download was sucessfull path to file, else None.
+    Check if a save path already exists and create save directory if it is 
+    not already present. Return True if file already exists, else False.
     """
     # Create the save directory if it does not yet exist
     Path(save_dir).mkdir(parents=True, exist_ok=True)
 
+    # Check if the file already exists
+    if os.path.isfile(save_path):
+        print(f"File already exists at {save_path}. Skipping {activity}.")
+        return True
+    
+    return False
+
+def download_file(url: str, save_dir: str, filename: str) -> str:
+    """
+    Download file from URL and save to the specified directory. Return
+    the path to the saved file if the download was completed or the file
+    already existed, else None.
+    """
     save_path = os.path.join(save_dir, filename)
 
-    # If file already exists locally, skip the download
-    if os.path.isfile(save_path):
-        print(f"File already exists at {save_path}. Skipping download.")
+    # If file already exists, do not download it again
+    if save_path_exists(save_path, save_dir, "download"):
         return save_path
     
     # Download file from the specified URL
@@ -45,44 +54,34 @@ def download_file(url: str, save_dir: str, filename: str) -> str:
         print(f"Failed to download file: {e}")
         return None
 
-def extract_ars(excel_path: str, save_dir: str, filename: str) -> None:
+def extract_ars(input_filepath: str, save_dir: str, filename: str) -> None:
     """
-    Extract the ARS for all German municipalities from the excel at the 
+    Extract ARS for all German municipalities from the input excel at the
     specified location and save as csv.
-
-    :param excel_filepath: path to locally stored excel file.
-    :param save_dir: directory where the csv will be saved.
-    :param filename: name of the file to save.
-    :return: None
     """
-    # Create the save directory if it does not yet exist
-    Path(save_dir).mkdir(parents=True, exist_ok=True)
     save_path = os.path.join(save_dir, filename)
 
-    # If file already exists, skip the extraction
-    if os.path.isfile(save_path):
-        print(f"File already exists at {save_path}. Skipping extraction.")
-        return None
+    # If file already exists, do not extract ARS again
+    if save_path_exists(save_path, save_dir, "extraction"):
+        return save_path
 
     # Extract ARS from excel file and save to CSV
     try:
-        df = pd.read_excel(excel_path)
-        column_data = df.iloc[7:, 1].dropna()  
-        column_data.to_csv(save_path, index=False, header=False)
-        print(f"CSV with ARS saved to: {save_path}")
+        df = pd.read_excel(input_filepath)
+        column_data = df.iloc[7:, [1, 2]].dropna()
+        column_data.columns = ["ARS", "Municipality"]
+        column_data["ARS"] = column_data["ARS"].astype(str)  
+        column_data.to_csv(save_path, index=False)
+        print(f"CSV with ARS saved to: {save_path}")    
     except Exception as e:
         print(f"Failed to extract ARS: {e}")
 
-def main():
-    EXCEL_URL = "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis:bevoelkerungsstatistik:schluessel:rs_2024-10-31/download/Regionalschl_ssel_2024-10-31.xlsx"  
+if __name__ == "__main__":
+    output_save_dir = "scraping/data/ars"
     
-    save_dir = "scraping/data/ars"
-    
-    filepath = download_file(EXCEL_URL, save_dir, filename="ars_raw.xlsx")
+    # Download the excel file containing the ARS
+    input_filepath = download_file(EXCEL_URL, output_save_dir, filename="ars_raw.xlsx")
     
     # If the download was successful, extract the ARS
-    if filepath:
-        extract_ars(filepath, save_dir, filename="ars_only.csv")
-
-if __name__ == "__main__":
-    main()
+    if input_filepath:
+        extract_ars(input_filepath, output_save_dir, filename="ars_clean.csv")
