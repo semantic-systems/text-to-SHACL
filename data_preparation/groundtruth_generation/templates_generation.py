3ec84bedcb3139b1f303b6f3d4fc08f0a5198387
Seike Appold <seike.appold@stud.leuphana.de>
2024-12-13 14:46:32 +0100

    Update repo structure

--- data_preparation/groundtruth_generation/templates_generation.py
+++ data_preparation/groundtruth_generation/templates_generation.py
@@ -14,79 +14,66 @@
 import os
 import json
 import sys
+import textwrap
 from typing import List
 
 
 def extract_benefit_information(input_file_path: dict) -> List[str]:
-    """Extracts information from a full social benefit description.
+    """Extracts information from a social benefit description.
 
-    :param input_file_path: Path to full benefit description.
+    :param input_file_path: Path to benefit description.
 
-    :return: List containing name, IDLB and requirements for the benefit.
+    :return: List with name, IDLB, and requirements text for the benefit.
     """
     # Load full benefit description
     with open(input_file_path, 'r', encoding='utf-8') as f:
-        social_benefit_description = json.load(f)
+        benefit_description = json.load(f)
 
     # Extract name, IDLB and requirements
-    name = social_benefit_description["name"].split(" ")[0].capitalize()
-    idlb = social_benefit_description["id"].replace(".", "_")
-    requirements = ""
-    for detail in social_benefit_description["details"]:
-        if detail["title"] == "Voraussetzungen":
-            requirements = detail["text"]
+    name = benefit_description["short_name"]
+    idlb = benefit_description["idlb"]
+    requirements = benefit_description["requirements"]
 
     return name, idlb, requirements
 
 
 def generate_shacl_template(name: str, idlb: str, output_dir: str) -> None:
-    """Generates a SHACL template.
-
-    Based on information about a social benefit, generate a template for
-    a SHACL shapes graph representing the eligibility requirements and
-    save to the specified location.
+    """Generates a SHACL template with some standard components.
 
     :param name: Name of the social benefit.
     :param idlb: Unique ID for the social benefit.
     :param output_dir: Directory where the generated template will be saved.
-
-    :return: None
     """
-    template_content = f"""@prefix sh: <http://www.w3.org/ns/shacl#> .
-    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-    @prefix ff: <https://foerderfunke.org/default#> .
+    template_content = textwrap.dedent(f"""\
+        @prefix sh: <http://www.w3.org/ns/shacl#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix ff: <https://foerderfunke.org/default#> .
 
-    #################################################################
-    #    METADATA
-    #################################################################
+        # METADATA
 
-    ff:{idlb} a ff:SocialBenefit ;
-        rdfs:label "{name}" .
+        ff:{idlb} a ff:SocialBenefit ;
+            rdfs:label "{name}" .
 
-    #################################################################
-    #    CONSTRAINTS
-    #################################################################
+        # CONSTRAINTS
 
-    ff:{name}Shape a sh:NodeShape ;
-        ff:checksFundingRequirement ff:{idlb} .
-    """
+        ff:{name}Shape a sh:NodeShape ;
+            ff:checksFundingRequirement ff:{idlb} .
+        """)
 
-    output_path = os.path.join(output_dir, f"{idlb}_true.ttl")
+    output_path = os.path.join(output_dir, f"{name.lower()}_gold.ttl")
     with open(output_path, 'w', encoding='utf-8') as f:
         f.write(template_content)
 
     print(f"SHACL template saved successfully at {output_path}")
 
 
-def generate_decomposition_template(requirements: str, output_dir: str) -> None:
+def generate_decomposition_template(name: str, requirements: str, output_dir: str) -> None:
     """Saves the requirements in a Markdown file for manual decomposition.
 
     :param requirements: Textual requirements for the social benefit.
     :param output_dir: Directory where the markdown file will be saved.
-
-    :return: None
     """
-    output_path = os.path.join(output_dir, "requirements.md")
+    output_path = os.path.join(output_dir, f"{name.lower()}_decomposed.md")
     with open(output_path, 'w', encoding='utf-8') as f:
         f.write("<b>Original requirements text</b>\n\n")
         f.write(requirements)
@@ -108,4 +95,4 @@ if __name__ == "__main__":
 
         # Generate SHACL template and decomposition template
         generate_shacl_template(name, idlb, output_dir_groundtruth)
-        generate_decomposition_template(requirements, output_dir_decomposition)
+        generate_decomposition_template(name, requirements, output_dir_decomposition)
