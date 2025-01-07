94eefaae85d6af0968691b36874cdd93dd82bbfb
Seike Appold <seike.appold@stud.leuphana.de>
2025-01-07 16:19:07 +0100

    Improve readability and robustness

--- src/generation/run_generation.py
+++ src/generation/run_generation.py
@@ -9,12 +9,13 @@ run_generation.py
 """
 
 from src.generation.generator_classes.api_access import ChatAIHandler
-from src.generation.generator_classes.gen_prompt import PromptComponents
+from src.generation.generator_classes.prompt_template import PromptComponents
 from langchain_openai.chat_models.base import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from typing import Tuple, Dict, Any
 from datetime import datetime
 import rdflib
+import argparse
 import json
 import sys
 import os
@@ -30,13 +31,13 @@ def parse_output(raw_output: str) -> str:
     else:
         data = raw_output.strip()
     
-    # Check if the data is parsable Turtle
+    # If data is not parsable, return empty string
     graph = rdflib.Graph()
     try:
         graph.parse(data=data, format="turtle")
         return data
     except Exception as e:
-        return f"# No parsable Turtle content found"
+        return ""
 
 
 def process_file(ontology_path: str, instruction_path: str, test_path: str, train_dir: str, model: ChatOpenAI, mode: str) -> Tuple[str, str]:
@@ -63,35 +64,39 @@ def process_file(ontology_path: str, instruction_path: str, test_path: str, trai
         response.response_metadata["examples"] = components.examples.examples_list
     # Set output manually if an error occurs during generation
     except Exception as e:
-        print(f"{e}. Mode: {mode}. Model: {model.model_name}. File: {os.path.basename(test_path)}")
+        print(f"Exception: {e}")
         return "", str(e), prompt
     
     return response.content, response.response_metadata, prompt
 
 
-def save_output(raw_dir: str, parsed_dir: str, prompt: str, prompt_id: str, metadata: Dict[str, Any], response: str, ) -> None:
-    """Save raw and parsed outputs."""
+def save_output(raw_dir: str, parsed_dir: str, prompt: str, prompt_id: str, metadata: Dict[str, Any], response: str) -> None:
+    """Save raw and parsed model outputs."""
     # Ensure directories exist
     os.makedirs(raw_dir, exist_ok=True)
     os.makedirs(parsed_dir, exist_ok=True)
 
     # Save raw output
-    with open(os.path.join(raw_dir, f"{prompt_id}.json"), "w", encoding="utf-8") as json_file:
+    raw_output_path = os.path.join(os.path.join(raw_dir, f"{prompt_id}.json"))
+    with open(raw_output_path, "w", encoding="utf-8") as json_file:
         json.dump({"prompt_id": prompt_id, "prompt": prompt, "response_metadata": metadata, "response": response}, 
                   json_file, indent=4, ensure_ascii=False,)
-    print(f"Raw output saved to {raw_dir}/{prompt_id}.json")
-     
+    print(f"Raw output saved to {raw_output_path}")
+    
+    # Save parsed output 
     if response:
         parsed_response = parse_output(response)
-        benefit = prompt_id.rsplit("_", 1)[-1]
-        with open(os.path.join(parsed_dir, f"{benefit}_gen.ttl"), "w", encoding="utf-8") as json_file:
+        parsed_output_path = os.path.join(parsed_dir, f"{prompt_id}.ttl")
+        #benefit = prompt_id.rsplit("_", 1)[-1]
+        with open(os.path.join(parsed_output_path), "w", encoding="utf-8") as json_file:
             json_file.write(parsed_response)
-        print(f"Parsed output saved to {parsed_dir}/{benefit}_gen.ttl")
+        print(f"Parsed output saved to {parsed_output_path}")
 
 def main(test_dir: str, train_dir: str, prompt_dir: str, output_dir: str, mode: str) -> None:
     """Generate SHACL from social benefit eligibility requirements with specified prompting technique.
     
-    :param input_dir: Directory containing input files with eligibility requirements.
+    :param test_dir: Directory with benefit descriptions used for testing.
+    :param train_dir: Directory with benefit descriptions used as examples during prompting.
     :param prompt_components_dir: Directory containing ontology and instruction files.
     :param model_output_dir: Directory to save the output.
     :param mode: Prompting method used for generating SHACL shapes ('baseline' or 'fewshot').
@@ -117,12 +122,12 @@ def main(test_dir: str, train_dir: str, prompt_dir: str, output_dir: str, mode: 
         raise FileNotFoundError(f"Instruction file not found: {instruction_path}")
     
     # Set up directories for raw and parsed output
-    raw_dir = os.path.join(output_dir, "raw_output")
+    raw_dir = os.path.join(output_dir, "raw_output", mode)
     parsed_dir = os.path.join(output_dir, mode)
 
     # Generate SHACL shapes for each test file with each model
     for model_index, model in enumerate(models):
-        # If there more than one model is used, create separate output directories
+        # If more than one model is used, create separate output directories
         model_parsed_dir = parsed_dir if len(models) == 1 else os.path.join(parsed_dir, model.model_name)
         for file_index, test_file in enumerate(os.listdir(test_dir)):
             print(f"Processing {os.path.basename(test_file)} (File {file_index+1}/{len(os.listdir(test_dir))}) " +
@@ -133,15 +138,14 @@ def main(test_dir: str, train_dir: str, prompt_dir: str, output_dir: str, mode: 
             save_output(raw_dir, model_parsed_dir, prompt, prompt_id, metadata, response)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 6:
-        print("Usage: python run_generation.py <test_dir> <train_dir> <prompt_dir> <output_dir> <mode>")
-        sys.exit(1)
-    
-    test_dir = sys.argv[1]
-    train_dir = sys.argv[2]
-    prompt_dir = sys.argv[3]
-    output_dir = sys.argv[4]
-    mode = sys.argv[5]  # 'baseline' or 'oneshot' or 'fewshot'
+    parser = argparse.ArgumentParser(description="Generate SHACL shapes from benefit descriptions using the specified prompting method.")
 
-    print(f"Starting generation with mode: {mode}")
-    main(test_dir, train_dir, prompt_dir, output_dir, mode)
+    parser.add_argument("test_dir", help="Directory with benefit descriptions used for testing.")
+    parser.add_argument("train_dir", help="Directory with benefit descriptions used as examples during prompting.")
+    parser.add_argument("prompt_components_dir", help="Directory containing ontology and instruction files.")
+    parser.add_argument("model_output_dir", help="Directory to save the output.")
+    parser.add_argument("mode", choices=["baseline", "oneshot", "fewshot"], help="Prompting method used for generating SHACL shapes ('baseline' or 'fewshot').")
+
+    args = parser.parse_args()
+
+    main(args.test_dir, args.train_dir, args.prompt_components_dir, args.model_output_dir, args.mode)
