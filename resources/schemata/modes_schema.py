3a5910ec3084957280e841ae9b75becbb4ddcbee
Seike Appold <seike.appold@stud.leuphana.de>
2025-01-21 17:25:02 +0100

    Add supported modes schema

---
+++ resources/schemata/modes_schema.py
@@ -0,0 +1,18 @@
+"""  
+modes_schema.py
+
+Information about supported modes for inference.
+"""
+
+supported_modes = {
+    "baseline": {
+        "description": "Prompt includes instruction + ontology, inference is executed with multiple models.",
+        "synonyms": ["default", "base"],
+        "models": ['meta-llama-3.1-8b-instruct', 'llama-3.1-nemotron-70b-instruct', 'llama-3.1-sauerkrautlm-70b-instruct', 'mistral-large-instruct', 'qwen2.5-72b-instruct']
+    },
+    "fewshot": {
+        "description": "Prompt includes instruction + ontology + variable number of randomly selected examples from train split, inference is executed with main model only.",
+        "synonyms": ["xshotrandom", "xshot"],
+        "models": ['mistral-large-instruct']
+    }
+}
